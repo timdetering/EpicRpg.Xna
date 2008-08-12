65a41f50284d2015ab59a2b333967da70d523dea
mitch.mgtzx <mitch.mgtzx@d3f98c10-4354-0410-ad24-d74515187f0f>
2008-08-12 15:26:42 +0000

    Configuration start

---
+++ trunk/EpicRPG/Managers/FileManager.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using EpicRPG.Util;
+
+namespace EpicRPG.Managers
+{
+    public class FileManager : Singleton<FileManager>
+    {
+        public void loadGameConfiguration(){
+            //TODO: look at game config file, load necessary configurations
+        }
+
+        public void parseConfigurationFile(string filename){
+
+        }
+    }
+}
