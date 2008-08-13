08af205544417fd0fc091b186b3d16ce6292414b
mitch.mgtzx <mitch.mgtzx@d3f98c10-4354-0410-ad24-d74515187f0f>
2008-08-13 22:45:52 +0000

    Test class; probably will be removed...

---
+++ trunk/EpicRPG/Util/WritableObject.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EpicRPG.Util
+{
+    public class WritableObject
+    {
+        public void ToXML(){
+
+        }
+    }
+}
